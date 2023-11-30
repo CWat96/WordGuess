4e2e9fa24ba8834c6e570b9af982596c84f2a13d
Ces <ces@Zipcoders-MacBook-Pro-42.local>
2023-11-30 08:11:47 -0500

    Final

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington;
-
+import java.io.InputStream;
+import java.util.Scanner;
 
 /**
  * @author xt0fer
@@ -7,4 +8,16 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
-}
+        public static void main(String[] args) {
+            final InputStream inputSource = System.in;
+            final Scanner scanner = new Scanner(inputSource); // allows us to get input
+            System.out.println("Enter a word to be guessed:");
+            final String secretWord = scanner.nextLine();
+            String guessedWord = null;
+            while(!secretWord.equals(guessedWord)) {
+                System.out.println("Enter your guess!");
+                guessedWord = scanner.nextLine();
+            }
+            System.out.println("You entered the correct value!");
+        }
+    }
